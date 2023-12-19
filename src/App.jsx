3e56dc644f7e21c7c48f624dae6e6e0b6c2fafa6
test <test@UGSERVER.com>
2023-12-19 16:10:09 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from "@material-tailwind/react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button color='blue' >Hello</Button>
+    </>
+  )
+}
+
+export default App
