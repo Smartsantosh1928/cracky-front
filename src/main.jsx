3f361d23d185611e5705b6d00b6d5603bb417a53
test <test@UGSERVER.com>
2023-12-26 16:10:49 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import Store from './store/Store.jsx';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <Provider store={Store} >
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Provider>
     </Router>
   </React.StrictMode>
 )
