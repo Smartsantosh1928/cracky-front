9e38f7769c53e89e582502d963695bebbbf4a47c
test <test@UGSERVER.com>
2023-12-28 11:22:37 +0530

    new

--- src/components/ShopCards.jsx
+++ src/components/ShopCards.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import ShopCard from './ShopCard';
 
 export function ShopCards() {
-    return <div className='lg:p-8 gap-5 grid grid-cols-3 lg:grid-cols-4 z-40 lg:-mt-[350px]'>
+    return <div className='lg:p-8 gap-5 grid grid-cols-1 lg:grid-cols-4 z-40 lg:-mt-[350px]'>
         <ShopCard/>
         <ShopCard/>
         <ShopCard/>
