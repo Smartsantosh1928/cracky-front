18accca5f436b2fc987813dbdbee0344ff676fdc
smartsantosh1928 <smartsantosh1928@gmail.com>
2024-01-03 21:45:41 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,11 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { ToggleDevice } from "./store/DeviceSlice";
 import { useGoogleOneTapLogin } from 'react-google-one-tap-login';
-import { SetUser, RemoveUser } from "./store/UserSlice";
-import Cookies from 'universal-cookie';
 
 function App() {
 
   const dispatch = useDispatch();
   const user = useSelector(state => state.user.user);
-  const cookies = new Cookies();
   
     // useGoogleOneTapLogin({
     //   onError: error => {
@@ -42,18 +39,7 @@ function App() {
     handleResize();
     window.addEventListener('resize', handleResize);
 
-    const accessToken = cookies.get('accessToken');
-    const refreshToken = cookies.get('refreshToken');
-
-    if (accessToken && refreshToken) {
-      sessionStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("refreshToken", refreshToken);
-    }
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      clearInterval(timeoutId);
-    }; 
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
--- src/components/Authenticator.jsx
+++ src/components/Authenticator.jsx
@@ -1,19 +1,16 @@
 import {
     Dialog,
-    DialogHeader,
     DialogBody,
-    DialogFooter,
     Card,
     CardBody,
     CardHeader,
     Typography,
     Button,
-    IconButton,
     Input,
     Checkbox,
     CardFooter,
 } from "@material-tailwind/react";
-import { LoginAuth, LogoutAuth, RegisterAuth } from '../store/AuthSlice';
+import { LoginAuth, LogoutAuth, RegisterAuth, ForgetPassword } from '../store/AuthSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { ReactSVG } from 'react-svg';
 import { Link } from "react-router-dom";
@@ -27,12 +24,15 @@ import PasswordStrengthBar from "react-password-strength-bar"
 import { PiEyeSlashThin, PiEyeThin } from "react-icons/pi";
 import { BsShieldFillExclamation } from "react-icons/bs";
 import axios from "axios"
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 export function Authenticator() {
 
     const url = import.meta.env.VITE_URL;
     const dispatch = useDispatch();
     const auth = useSelector(state => state.auth.auth);
+    const isMobile = useSelector(state => state.device.isMobile);
 
     const [ details, setDetails ] = useState({email: "", password: "", confirmPassword: "", emailOffers: false, passwordOpen: false, confirmPasswordOpen: false, errors: true});
     const [ errors, setErrors ] = useState({ email: "", password: "", confirmPassword: "" });
@@ -53,19 +53,23 @@ export function Authenticator() {
     }
 
     useEffect(() => {
-        if(errors.email==""&&errors.password==""&&errors.confirmPassword==""){
-            setDetails(prevDetails => ({
-                ...prevDetails,
-                errors: false
-            }))
-        }else{
-            setDetails(prevDetails => ({
-                ...prevDetails,
-                errors: true
-            }))
+        let hasErrors = false;
+    
+        if (auth === "login") {
+            hasErrors = !(errors.email === "" && errors.password === "" && details.email !== "" && details.password !== "");
+        } else if (auth === "forgetPassword") {
+            hasErrors = !(errors.email === "" && details.email !== "");
+        } else {
+            hasErrors = !(errors.email === "" && errors.password === "" && errors.confirmPassword === "" &&
+                details.email !== "" && details.password !== "" && details.confirmPassword !== "");
         }
-    },[errors])
-
+    
+        setDetails(prevDetails => ({
+            ...prevDetails,
+            errors: hasErrors
+        }));
+    }, [errors, auth]);
+    
     const validateDetails = (name, value) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       
@@ -126,18 +130,59 @@ export function Authenticator() {
     }
 
     const handleSignIn = () => {
-        console.log(details);
-
+        axios.post(url + "/auth/login", {
+            email: details.email,
+            password: details.password
+        })
+        .then((res) => {
+            const data = res.data;
+            console.log(data);
+            if (data.success) {
+                sessionStorage.setItem("accessToken", data.accessToken);
+                localStorage.setItem("refreshToken", data.refreshToken);
+                toast.success(data.message)
+                dispatch(LogoutAuth())
+            } else {
+                toast.error(data.message);
+            }
+        })
+        .catch((error) => {
+            if (error.response) {
+                const responseData = error.response.data;
+                toast.error(responseData.message);
+            } else {
+                toast.error("An error occurred");
+            }
+        });
     }
 
-    const handleSignUp = async () => {
-        const data = await axios.post(url+"/auth/register",{
+    const handleSignUp = () => {
+        axios.post(url + "/auth/register", {
             email: details.email,
             password: details.password,
             emailOffers: details.emailOffers
         })
-        console.log(data);
-    }
+        .then((res) => {
+            const data = res.data;
+            console.log(data);
+            if (data.success) {
+                sessionStorage.setItem("accessToken", data.accessToken);
+                localStorage.setItem("refreshToken", data.refreshToken);
+                toast.success(data.message)
+                dispatch(LogoutAuth())
+            } else {
+                toast.error(data.message);
+            }
+        })
+        .catch((error) => {
+            if (error.response) {
+                const responseData = error.response.data;
+                toast.error(responseData.message);
+            } else {
+                toast.error("An error occurred");
+            }
+        });
+    }    
 
     const handleForgetPassword = () => {
 
@@ -155,13 +200,25 @@ export function Authenticator() {
             }}
         >
             <DialogBody className="relative grid place-items-center gap-4 ">
+            <ToastContainer
+                position="bottom-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
             <Card className={`w-full flex flex-row justify-between pb-5 font-secondary bg-[#f1eaff] `} >
-            <IoIosCloseCircleOutline className="w-7 h-7 absolute -top-12 -right-12 bg-white rounded-full cursor-pointer z-50" onClick={() => dispatch(LogoutAuth())} />
-            <CardHeader
+            <IoIosCloseCircleOutline className={`w-7 h-7 absolute -top-16 -right-8 md:-top-12 md:-right-12 bg-white rounded-full cursor-pointer z-50`} onClick={() => dispatch(LogoutAuth())} />
+            {!isMobile && <CardHeader
                     shadow={true}
                     floated={false}
                     className={`w-full h-full z-50  transition-all  duration-[700ms] ${auth=="login" ? 'translate-x-0 bg-white' : 'bg-white translate-x-full'} ${auth=='login' ? 'animate-slideLeft' : 'animate-slideRight'} `}
-                > { auth=="login" ?
+                >   {auth=="login" &&
                     (<div className=" flex flex-col justify-center  ">
                     <Typography variant="h3" color="blue" className="w-full h-full mb-3 mt-5  text-center items-center font-primary">
                         Login
@@ -171,7 +228,19 @@ export function Authenticator() {
                     </Typography>
                     <ReactSVG className="" loading={() => <span>Loading...</span>} src={signin}>
                         SVG is not supported
-                    </ReactSVG></div>):(
+                    </ReactSVG></div>)}
+                    {auth=="forgetPassword" &&
+                    (<div className=" flex flex-col justify-center  ">
+                    <Typography variant="h3" color="blue" className="w-full h-full mb-3 mt-5  text-center items-center font-primary tracking-wider">
+                        Reset Password
+                    </Typography>
+                    <Typography variant="paragraph" className="font-secondary text-center px-5" >
+                        Enter your email address to receive Password Reset link
+                    </Typography>
+                    <ReactSVG className="" loading={() => <span>Loading...</span>} src={signin}>
+                        SVG is not supported
+                    </ReactSVG></div>)}
+                    {auth=="register"&&(
                     <div className="">
                     <Typography variant="h3" color="deep-orange" className="w-full h-full mb-3 text-center items-center font-primary mt-5">
                         Sign Up
@@ -182,13 +251,17 @@ export function Authenticator() {
                     <ReactSVG className="md:p-9 " loading={() => <span>Loading</span>} src={signup}>
                         SVG is not supported
                     </ReactSVG>
-                    {/* <img src={`/lotties/sign-up.svg`}  className={`md:p-8`} alt="" /> */}
                     </div>)}
-                </CardHeader>
+                </CardHeader>}
                 
-                <div className={`w-full h-full justify-center flex flex-col transition-transform duration-[700ms] ${auth=='login' ? 'translate-x-0' : '-translate-x-full'} ${auth=='login' ? 'animate-slideLeft' : 'animate-slideRight'}`}>
-                {auth=="login" ?(
-                    <div className="mt-10">
+                <div className={`w-full h-full justify-center flex flex-col transition-transform duration-[700ms] ${auth=='login' ? 'translate-x-0' : 'md:-translate-x-full'} ${auth=='login' ? 'animate-slideLeft' : 'md:animate-slideRight'}`}>
+                {isMobile && <Typography variant="h4" color={auth=="register"? "deep-orange" :"blue"} className="w-full text-center font-secondary mt-2" >
+                        {auth=="login" && "Login"}
+                        {auth=="forgetPassword" && "Reset Password"}
+                        {auth=="register" && "Sign Up"}
+                    </Typography>}
+                {auth=="login" &&(
+                    <div className="">
                     <CardBody className="flex flex-col gap-3">
                         <Input label="Email" variant="outlined" size="lg" value={details.email} name="email" onChange={handleChange}/>
                         {errors.email!="" && <Typography
@@ -210,7 +283,7 @@ export function Authenticator() {
                             variant="small"
                             color="blue"
                             className="w-full h-full flex justify-end cursor-pointer font-secondary"
-                            onClick={handleForgetPassword}
+                            onClick={() => dispatch(ForgetPassword())}
                         >
                             Forget Password ?
                         </Typography>
@@ -248,8 +321,54 @@ export function Authenticator() {
                             </div>
                         </div>
                     </CardFooter>
-                </div>):(
-                    <div className="h-full w-full -ml-5 mt-3 flex flex-col justify-center items-center">
+                </div>)}
+                {auth=="forgetPassword" &&(
+                    <div className="-ml-10 md:px-3">
+                    <CardBody className="flex flex-col gap-3">
+                        <Input label="Email" size="lg" name="email" value={details.email} onChange={handleChange} />
+                        {errors.email!="" && <Typography
+                            variant="small"
+                            className="-mt-[10px] ml-1 flex items-center gap-1 font-normal text-red-400"
+                        >
+                            <BsShieldFillExclamation className="w-3 h-3" />{errors.email}
+                        </Typography>}
+                    </CardBody>
+                    <CardFooter className="py-0 flex flex-col gap-3">
+                        <Button variant="gradient" color="blue" disabled={details.errors} onClick={handleForgetPassword} fullWidth className="font-secondary flex justify-center items-center">
+                            Send Reset Link
+                            <IoIosArrowRoundForward className="w-6 h-6 ml-1"/>
+                        </Button>
+                        <Typography variant="paragraph" className="mt-2 flex justify-center">
+                            New to Cracky?
+                            <Typography
+                                variant="paragraph"
+                                color="blue"
+                                className="ml-1 font-secondary cursor-pointer"
+                                onClick={() => dispatch(RegisterAuth())}
+                            >
+                            Create an account
+                            </Typography>
+                        </Typography>
+                        <div className="w-full h-full flex justify-center items-center flex-col">
+                            <span className="border border-b-0 border-blue-gray-300 w-[90%]"></span>
+                            <div className="w-full h-full mt-4 flex justify-center items-center gap-5">
+                                <a href={ `${url}/auth/google`}>
+                                    <Card className="p-3 cursor-pointer" >
+                                        <FcGoogle className="w-6 h-6" />
+                                    </Card>
+                                </a>
+                                <a href={ `${url}/auth/facebook`}>
+                                    <Card className="p-3 cursor-pointer" >
+                                        <FaFacebook className="w-6 h-6 text-blue-500" />
+                                    </Card>
+                                </a>
+                            </div>
+                        </div>
+                    </CardFooter>
+                </div>
+                )}
+                {auth=="register"&&(
+                    <div className="-ml-10 md:px-3">
                     <CardBody className="flex flex-col gap-3">
                         <Input label="Email" size="lg" name="email" value={details.email} onChange={handleChange} />
                         {errors.email!="" && <Typography
