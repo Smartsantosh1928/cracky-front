c50742d8133e5906d81929b13f3fea853a5bad63
karmegamm <test@UGSERVER.com>
2024-01-08 12:02:34 +0530

    new

--- src/components/seller/Sbanner.jsx
+++ src/components/seller/Sbanner.jsx
@@ -9,7 +9,8 @@ import { IoHome } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 import { BsChevronDoubleRight } from "react-icons/bs";
 import { HiOutlineUserGroup } from "react-icons/hi";
-import { SlWallet } from "react-icons/sl";
+import { PiWalletLight,PiChartLineDownThin,PiHeadphonesLight,PiHandbagThin } from "react-icons/pi";
+import { LiaCartArrowDownSolid } from "react-icons/lia";
 export function SBanner() {
      return (
      <>
@@ -29,28 +30,31 @@ export function SBanner() {
                 </div>
             </div>
             <div className='flex justify-center items-center'>
-                <Card shadow={false} className='shado drop-shadow-xl w-[75%]'>
-                    <CardBody className='w-full flex justify-between '>
-                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center'>
-                            <HiOutlineUserGroup strokeWidth={1} className='text-blue-600 h-12 w-12'/>
-                            <Typography className='font-secondary text-black/80 text-center'>45 crore+ Flipkart  customers</Typography>
+                <Card shadow={false} className='drop-shadow-xl w-[85%]'>
+                    <CardBody className='w-full flex justify-between    '>
+                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center px-6'>
+                            <HiOutlineUserGroup strokeWidth={1.5} className='text-blue-600 h-12 w-12'/>
+                            <Typography className='font-secondary text-black/80 text-center'>45 crore+  Cracky customers</Typography>
                         </div>
-                        <span className='w-.0 h-1 bg-black'></span>
-                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center'>
-                            <SlWallet    strokeWidth={1} className='text-blue-600 h-12 w-12'/>
-                            <Typography className='font-secondary text-black/80 text-center'>45 crore+ Flipkart  customers</Typography>
+                        <span className='min-w-[2px] max-h-[45px] opacity-40 mt-9 bg-blue-gray-500'></span>
+                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center px-6'>
+                            <PiWalletLight    strokeWidth={1.5} className='text-blue-600 h-12 w-12'/>
+                            <Typography className='font-secondary text-black/80 text-center'>7* days secure & regular payments</Typography>
                         </div>
-                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center'>
-                            <HiOutlineUserGroup strokeWidth={1} className='text-blue-600 h-12 w-12'/>
-                            <Typography className='font-secondary text-black/80 text-center'>45 crore+ Flipkart  customers</Typography>
+                        <span className='min-w-[2px] max-h-[45px] opacity-40 mt-9 bg-blue-gray-500'></span>
+                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center px-6'>
+                            <LiaCartArrowDownSolid  strokeWidth={0} className='text-blue-600 h-12 w-12'/>
+                            <Typography className='font-secondary text-black/80 text-center'>Low cost of doing business</Typography>
                         </div>
-                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center'>
-                            <HiOutlineUserGroup strokeWidth={1} className='text-blue-600 h-12 w-12'/>
-                            <Typography className='font-secondary text-black/80 text-center'>45 crore+ Flipkart  customers</Typography>
+                        <span className='min-w-[2px] max-h-[45px] opacity-40 mt-9 bg-blue-gray-500'></span>
+                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center px-10'>
+                            <PiHeadphonesLight  strokeWidth={2} className='text-blue-600 h-12 w-12'/>
+                            <Typography className='font-secondary text-black/80 text-center'>One click Seller Support</Typography>
                         </div>
-                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center'>
-                            <HiOutlineUserGroup strokeWidth={1} className='text-blue-600 h-12 w-12'/>
-                            <Typography className='font-secondary text-black/80 text-center'>45 crore+ Flipkart  customers</Typography>
+                        <span className='min-w-[2px] max-h-[45px] opacity-40 mt-9 bg-blue-gray-500'></span>
+                        <div className='w-1/5 flex flex-col items-center gap-3 justify-center px-6'>
+                            <PiHandbagThin strokeWidth={8} className='text-blue-600 h-12 w-12'/>
+                            <Typography className='font-secondary text-black/80 text-center'>Access to The Big Billion Days & more</Typography>
                         </div>
                     </CardBody>
                 </Card>
