dfec99cab3a87003dd0e7be7720fb91ead1f9595
Santhosh775 <s.santhosh7222@gmail.com>
2024-01-10 09:26:39 +0530

    doc

---
+++ src/documentation/seller/sellonline/createaccount.jsx
@@ -0,0 +1,15 @@
+import { Card, Typography } from "@material-tailwind/react";
+
+
+export function Sellonline(){
+    return(
+        <> 
+            <Card> 
+                <div className="flex justify-center items-center">
+                    <Typography>Create Account</Typography>
+                </div>
+            </Card>
+        </>
+    );
+}
+export default Sellonline
