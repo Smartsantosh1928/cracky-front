2b164fee1c8511f1d62bca2fe347835c5c1bf631
test <test@UGSERVER.com>
2024-02-24 10:41:38 +0530

    new

--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -7,9 +7,12 @@ import { Card,
 } from '@material-tailwind/react';
 import { MdMarkEmailRead } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
-import { CgProfile } from "react-icons/cg";
+// import { CgProfile } from "react-icons/cg";
 import axios from "../../utils/axios.config"
 import { Link } from 'react-router-dom';
+import { FaSquarePhone } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
 
 export function Profile() {
     
@@ -41,7 +44,7 @@ export function Profile() {
                 <div className='w-full h-full flex flex-col gap-5 mt-6'>
                     <div className='font-secondary flex'>
                         <img src={details.profilePictureUrl} alt="profile Picture" className='w-28 h-28 rounded-full' />
-                        <div className='p-4'>
+                        <div className='p-4 flex justify-start flex-col items-start'>
                             <div className='flex gap-5'>
                                 <Typography className='font-secondary text-2xl' >{details.name}</Typography>
                                 <div>   
@@ -52,7 +55,29 @@ export function Profile() {
                                 <MdMarkEmailRead className={iconClass} />
                                 <Typography className='font-secondary text-lg' >{details.email}</Typography>
                             </div>
+                            <div className='flex gap-2 justify-center items-center'>
+                                <FaSquarePhone className={iconClass} />
+                                <Typography className='font-secondary text-lg' >{details.phoneNumber}</Typography>
+                            </div>
                         </div>
+                    </div>
+                    <hr />
+                    <div className='flex flex-col gap-5 justify-start items-start'>
+                        {/* <div className='flex gap-5 justify-start items-start'>
+                            <Typography className='font-secondary text-2xl' >Connections</Typography>
+                            <div className='grid grid-cols-3 gap-5'>
+                                {details?.providers?.map((provider,key) => <Card key={key} className='flex justify-center items-center flex-row gap-3 p-2 md:-mt-3'>
+                                {provider=="google" && <FcGoogle className='w-6 h-6' />}
+                                {provider=="facebook" && <FaFacebook className='w-6 h-6' />}
+                                {provider=="email" && <MdMarkEmailRead className='w-6 h-6' />}
+                                <Typography color="blue-gray" className='text-xl hidden md:block' >{provider.charAt(0).toUpperCase() + provider.slice(1)}</Typography>
+                                </Card>)}
+                            </div>
+                        </div>
+                        <div>
+                            <Typography className='font-secondary text-2xl' >Bio</Typography>
+                            <Typography className='font-secondary text-lg' ></Typography>
+                        </div> */}
                     </div>
                 </div>
             </CardBody>
