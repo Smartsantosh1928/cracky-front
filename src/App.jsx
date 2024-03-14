521a3d87b7e901ddb558d818cc33da4a830d32a9
Santhosh775 <s.santhosh7222@gmail.com>
2024-03-14 12:46:26 +0530

    ListForm

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { useGoogleOneTapLogin } from 'react-google-one-tap-login';
 import { Dashboard } from "./layout/user/Dashboard"
 import { SDashboard } from "./layout/seller/Dashboard"
 import { SellerRegister } from "./pages/seller";
+import ListForm from "./components/ListingPage/ListForm";
 
 import "react-toastify/dist/ReactToastify.css";
 import ListingPage from "./components/ListingPage/ListingPage";
@@ -49,14 +50,16 @@ function App() {
 
   return (
     <>
-      {/* <Routes>
+      <Routes>
         <Route path="/user/*" element={<Dashboard />} />
         <Route path="/seller/register" element={<SellerRegister />} />
         <Route path="/seller/*" element={<SDashboard />} />
+        <Route path="/ListingPage/*" element={<ListingPage/>} />
+        <Route path="/ListingPage/ListForm" element={<ListForm/>} />
         <Route path="/shop/:shop_id/*" element={<Dashboard />} />
         <Route path="/" element={<Navigate to="/user" replace />} />
-      </Routes> */}
-      <ListingPage />
+      </Routes>
+      
     </>
   )
 }
